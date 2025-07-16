eee399cffcf794f13ce496016d4894ba6214407f
AbiAbdurrahman <abi_abdurrahman@hotmail.com>
2025-07-16 08:31:01 +0700

    initiate about page

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import AboutBanner from '@/components/about-banner'
+import MediaAndPublishing from '@/components/media-and-publishing'
+import VisionMission from '@/components/vision-mission'
+
+const About = () => (
+  <>
+    <AboutBanner />
+    <VisionMission />
+    <MediaAndPublishing />
+  </>
+)
+
+export default About;
